11edb94ff8bfd78efa3c6992d214e4a8dc7bedcb
Yukun Zou <yukunzou@kth.se>
2023-02-21 23:01:35 +0100

    finish context of setting page.Still need to realize data pass to control slider range.

--- app/src/main/java/com/example/shouldiwearshortstoday/SettingsActivity.kt
+++ app/src/main/java/com/example/shouldiwearshortstoday/SettingsActivity.kt
@@ -21,45 +21,52 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting)
 
-        // Get a reference to the "Show Popup" button
         val showPopupButton = findViewById<Button>(R.id.setting_popup)
 
-        // Set a click listener for the "Show Popup" button
         showPopupButton.setOnClickListener {
-            // Inflate the popup layout
             val popupView = LayoutInflater.from(this).inflate(R.layout.setting_window, null)
 
-            // Create a new popup window with the specified layout
-            val popupWindow = PopupWindow(popupView, ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+            val popupWindow = PopupWindow(
+                popupView,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+            )
 
-            // Set a background drawable for the popup window
             popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-            // Set the focusable property of the popup window
             popupWindow.isFocusable = true
 
-            // Show the popup window at the center of the screen
-            popupWindow.showAtLocation(findViewById(R.id.setting_constraint_layout), Gravity.CENTER, 0, 0)
+            popupWindow.showAtLocation(
+                findViewById(R.id.setting_constraint_layout),
+                Gravity.CENTER,
+                0,
+                0
+            )
 
-            // Get a reference to the input box in the popup layout
-            val inputBox = popupView.findViewById<EditText>(R.id.input_box)
+            val startTimeEditText = popupView.findViewById<EditText>(R.id.edit_text_start_time)
+            val endTimeEditText = popupView.findViewById<EditText>(R.id.edit_text_end_time)
 
-            // Set a click listener for the "OK" button in the popup layout
-            popupView.findViewById<Button>(R.id.submit_button).setOnClickListener {
-                // Get the text from the input box
-                val inputText = inputBox.text.toString()
 
-                // Show a toast message with the input text
-                Toast.makeText(this, "Input Text: $inputText", Toast.LENGTH_SHORT).show()
+            val cancelButton = popupView.findViewById<Button>(R.id.button_cancel)
 
+            cancelButton.setOnClickListener {
                 // Dismiss the popup window
                 popupWindow.dismiss()
             }
+
+            popupView.findViewById<Button>(R.id.button_save).setOnClickListener {
+                val inputText1 = startTimeEditText.text.toString()
+                val inputText2 = endTimeEditText.text.toString()
+                Toast.makeText(this, "Input Text: $inputText1 $inputText2", Toast.LENGTH_SHORT).show()
+
+                popupWindow.dismiss()
+            }
         }
+
     }
+
     fun closeSettingsActivity(view: View) {
         finish()
     }
-
 }
+
 
