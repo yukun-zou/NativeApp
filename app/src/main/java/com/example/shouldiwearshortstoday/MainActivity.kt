1e08d6f9dc00574485f7df905dc7fd1ca25c7dab
Tim <sonnenpelzxx@gmail.com>
2023-02-22 11:36:09 +0100

    added coroutine

--- app/src/main/java/com/example/shouldiwearshortstoday/MainActivity.kt
+++ app/src/main/java/com/example/shouldiwearshortstoday/MainActivity.kt
@@ -2,13 +2,68 @@ package com.example.shouldiwearshortstoday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val weather = Weather()
-        weather.clothingAlgorithm(8,18)
-
+        val weatherCoroutine = lifecycleScope.async {
+            weather.clothingAlgorithm(8, 18)
+        }
+        weatherCoroutine.invokeOnCompletion{
+            setClothing(weatherCoroutine.getCompleted())
+        }
+    }
+    fun setClothingToInvisible(): Array<ImageView> {
+        val clothes = arrayOf(findViewById<ImageView>(R.id.hat),
+            findViewById<ImageView>(R.id.scarf),
+            findViewById<ImageView>(R.id.tshirt),
+            findViewById<ImageView>(R.id.hoodie),
+            findViewById<ImageView>(R.id.winterJacket),
+            findViewById<ImageView>(R.id.shorts),
+            findViewById<ImageView>(R.id.trousers),
+            findViewById<ImageView>(R.id.umbrella)
+        )
+        for (element in clothes){
+            element.visibility = View.INVISIBLE
+        }
+        return clothes
+    }
+    fun setClothing(weather: Array<String>){
+        val umbrella = weather[1]
+        val tempType = weather[0]
+        val clothes = setClothingToInvisible()
+        if(tempType == "freezing"){
+            clothes[0].visibility = View.VISIBLE
+            clothes[1].visibility = View.VISIBLE
+            clothes[4].visibility = View.VISIBLE
+            clothes[6].visibility = View.VISIBLE
+        }
+        else if(tempType == "cold"){
+            clothes[4].visibility = View.VISIBLE
+            clothes[6].visibility = View.VISIBLE
+        }
+        else if(tempType == "comfortable"){
+            clothes[3].visibility = View.VISIBLE
+            clothes[6].visibility = View.VISIBLE
+        }
+        else if(tempType == "warm"){
+            clothes[2].visibility = View.VISIBLE
+            clothes[6].visibility = View.VISIBLE
+        }
+        else{
+            clothes[2].visibility = View.VISIBLE
+            clothes[5].visibility = View.VISIBLE
+        }
+        if(umbrella == "true") {
+            clothes[7].visibility = View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/shouldiwearshortstoday/Weather.kt
+++ app/src/main/java/com/example/shouldiwearshortstoday/Weather.kt
@@ -1,11 +1,13 @@
 package com.example.shouldiwearshortstoday
 
+import android.widget.ImageView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
@@ -13,60 +15,79 @@ import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
-class Weather : ViewModel(){
-    suspend fun getWeatherAPI(lat: String, long: String): JSONObject {
+class Weather{
+    private suspend fun getWeatherAPI(lat: String, long: String): JSONObject {
         return withContext(Dispatchers.IO) {
-            var address = "https://api.open-meteo.com/v1/forecast?latitude=€LAT&longitude=€LONG&hourly=apparent_temperature,precipitation&current_weather=true"
+            var address =
+                "https://api.open-meteo.com/v1/forecast?latitude=€LAT&longitude=€LONG&hourly=apparent_temperature,precipitation&current_weather=true"
             address = address.replace("€LAT", lat)
             address = address.replace("€LONG", long)
             val connection = URL(address).openConnection() as HttpURLConnection
-            var text : String
+            var text: String
             try {
                 connection.connect();
-                text = connection.inputStream.use{it.reader().use{reader -> reader.readText()}}
-            }
-            finally {
+                text =
+                    connection.inputStream.use { it.reader().use { reader -> reader.readText() } }
+            } finally {
                 connection.disconnect()
             }
             return@withContext JSONObject(text)
         }
     }
-    suspend fun getLocationAPI(city: String): JSONObject {
+
+    private suspend fun getLocationAPI(city: String): JSONObject {
         return withContext(Dispatchers.IO) {
             var address = "https://geocoding-api.open-meteo.com/v1/search?name=€NAME"
             address = address.replace("€NAME", city)
             val connection = URL(address).openConnection() as HttpURLConnection
-            var text : String
+            var text: String
             try {
                 connection.connect();
-                text = connection.inputStream.use{it.reader().use{reader -> reader.readText()}}
-            }
-            finally {
+                text =
+                    connection.inputStream.use { it.reader().use { reader -> reader.readText() } }
+            } finally {
                 connection.disconnect()
             }
             return@withContext JSONObject(text)
         }
     }
-    fun clothingAlgorithm(start: Int, end: Int){
-        viewModelScope.launch(Dispatchers.IO) {
-            val locationResponse = getLocationAPI("Stockholm")
-            val lat = locationResponse.getJSONArray("results").getJSONObject(0).getString("latitude")
-            val long = locationResponse.getJSONArray("results").getJSONObject(0).getString("longitude")
-            val weatherResponse = getWeatherAPI(lat.toString(), long.toString())
-            val temperature = weatherResponse.getJSONObject("hourly").getJSONArray("apparent_temperature")
-            val precipitation = weatherResponse.getJSONObject("hourly").getJSONArray("precipitation")
-            var minTemp = temperature.getInt(start)
-            var maxPri = precipitation.getInt(start)
-            for (i in start..end){
-                if(temperature.getInt(i) < minTemp){
-                    minTemp = temperature.getInt(i)
-                }
-                if(precipitation.getInt(i) > maxPri){
-                    maxPri = precipitation.getInt(i)
-                }
+
+    suspend fun clothingAlgorithm(start: Int, end: Int): Array<String> {
+
+        val locationResponse = getLocationAPI("Stockholm")
+        val lat = locationResponse.getJSONArray("results").getJSONObject(0).getString("latitude")
+        val long = locationResponse.getJSONArray("results").getJSONObject(0).getString("longitude")
+        val weatherResponse = getWeatherAPI(lat.toString(), long.toString())
+        val temperature =
+            weatherResponse.getJSONObject("hourly").getJSONArray("apparent_temperature")
+        val precipitation = weatherResponse.getJSONObject("hourly").getJSONArray("precipitation")
+        var minTemp = temperature.getInt(start)
+        var maxPre = precipitation.getInt(start)
+        for (i in start..end) {
+            if (temperature.getInt(i) < minTemp) {
+                minTemp = temperature.getInt(i)
             }
-            maxPri
-            minTemp
+            if (precipitation.getInt(i) > maxPre) {
+                maxPre = precipitation.getInt(i)
+            }
         }
+        var clothingType: String
+        var umbrella: Boolean
+        if (minTemp < 0) {
+            clothingType = "freezing"
+        } else if (minTemp < 10) {
+            clothingType = "cold"
+        } else if (minTemp < 15) {
+            clothingType = "comfortable"
+        } else if (minTemp < 23) {
+            clothingType = "warm"
+        } else {
+            clothingType = "hot"
+        }
+        umbrella = (maxPre > 0)
+        val array = Array(2) { "" }
+        array[0] = clothingType
+        array[1] = umbrella.toString()
+        return array
     }
 }
